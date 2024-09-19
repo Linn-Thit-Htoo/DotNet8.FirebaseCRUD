86b27e749b34763f991e5d359a07ff5949aae1d6
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-19 15:28:08 +0630

    -

--- DotNet8.FirebaseCRUD.ConsoleApp/BlogModel.cs
+++ DotNet8.FirebaseCRUD.ConsoleApp/BlogModel.cs
@@ -1,10 +1,9 @@
-﻿namespace DotNet8.FirebaseCRUD.ConsoleApp
+﻿namespace DotNet8.FirebaseCRUD.ConsoleApp;
+
+public class BlogModel
 {
-    public class BlogModel
-    {
-        public string BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    public string BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
