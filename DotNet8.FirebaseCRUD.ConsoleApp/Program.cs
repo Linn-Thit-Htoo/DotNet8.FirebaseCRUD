6a13d20ffd25c66b0545a54419cac5d93c661cf0
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-19 15:30:31 +0630

    -

--- DotNet8.FirebaseCRUD.ConsoleApp/Program.cs
+++ DotNet8.FirebaseCRUD.ConsoleApp/Program.cs
@@ -1,34 +1,33 @@
-﻿namespace DotNet8.FirebaseCRUD.ConsoleApp
+﻿namespace DotNet8.FirebaseCRUD.ConsoleApp;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
+        BlogService blogService = new();
+        var blog = new BlogModel()
         {
-            BlogService blogService = new();
-            var blog = new BlogModel()
-            {
-                BlogId = Guid.NewGuid().ToString(),
-                BlogTitle = "Test Firebase Title edited",
-                BlogAuthor = "Test Firebase Author edited",
-                BlogContent = "Test Firebase Content edited"
-            };
+            BlogId = Guid.NewGuid().ToString(),
+            BlogTitle = "Test Firebase Title edited",
+            BlogAuthor = "Test Firebase Author edited",
+            BlogContent = "Test Firebase Content edited"
+        };
 
-            //await blogService.AddBlogAsync(blog);
-            //await blogService.UpdateBlogAsync("-O78Tno7NMVTn0OPt0Zr", blog);
-            //await blogService.DeleteBlogAsync("-O78Tno7NMVTn0OPt0Zr");
+        //await blogService.AddBlogAsync(blog);
+        //await blogService.UpdateBlogAsync("-O78Tno7NMVTn0OPt0Zr", blog);
+        //await blogService.DeleteBlogAsync("-O78Tno7NMVTn0OPt0Zr");
 
-            var blogs = await blogService.GetBlogsAsync();
-            if (blogs is not null && blogs.Count > 0)
+        var blogs = await blogService.GetBlogsAsync();
+        if (blogs is not null && blogs.Count > 0)
+        {
+            foreach (var item in blogs)
             {
-                foreach (var item in blogs)
-                {
-                    Console.WriteLine($"Key: {item.Key}");
-                    Console.WriteLine($"Blog Id: {item.Value.BlogId}");
-                    Console.WriteLine($"Blog Title: {item.Value.BlogTitle}");
-                    Console.WriteLine($"Blog Author: {item.Value.BlogAuthor}");
-                    Console.WriteLine($"Blog Content: {item.Value.BlogContent}");
-                    Console.WriteLine("------------------------------------");
-                }
+                Console.WriteLine($"Key: {item.Key}");
+                Console.WriteLine($"Blog Id: {item.Value.BlogId}");
+                Console.WriteLine($"Blog Title: {item.Value.BlogTitle}");
+                Console.WriteLine($"Blog Author: {item.Value.BlogAuthor}");
+                Console.WriteLine($"Blog Content: {item.Value.BlogContent}");
+                Console.WriteLine("------------------------------------");
             }
         }
     }
