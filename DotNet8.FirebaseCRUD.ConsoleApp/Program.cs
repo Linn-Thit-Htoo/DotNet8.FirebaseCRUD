f7d8643e3b459394a3a4cba2bc7d44d8a7fc1df8
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-19 15:30:25 +0630

    -

--- DotNet8.FirebaseCRUD.ConsoleApp/Program.cs
+++ DotNet8.FirebaseCRUD.ConsoleApp/Program.cs
@@ -1,31 +1,34 @@
-﻿public class Program
+﻿namespace DotNet8.FirebaseCRUD.ConsoleApp
 {
-    public static async Task Main(string[] args)
+    public class Program
     {
-        BlogService blogService = new();
-        var blog = new BlogModel()
+        public static async Task Main(string[] args)
         {
-            BlogId = Guid.NewGuid().ToString(),
-            BlogTitle = "Test Firebase Title edited",
-            BlogAuthor = "Test Firebase Author edited",
-            BlogContent = "Test Firebase Content edited"
-        };
-
-        //await blogService.AddBlogAsync(blog);
-        //await blogService.UpdateBlogAsync("-O78Tno7NMVTn0OPt0Zr", blog);
-        //await blogService.DeleteBlogAsync("-O78Tno7NMVTn0OPt0Zr");
-
-        var blogs = await blogService.GetBlogsAsync();
-        if (blogs is not null && blogs.Count > 0)
-        {
-            foreach (var item in blogs)
+            BlogService blogService = new();
+            var blog = new BlogModel()
             {
-                Console.WriteLine($"Key: {item.Key}");
-                Console.WriteLine($"Blog Id: {item.Value.BlogId}");
-                Console.WriteLine($"Blog Title: {item.Value.BlogTitle}");
-                Console.WriteLine($"Blog Author: {item.Value.BlogAuthor}");
-                Console.WriteLine($"Blog Content: {item.Value.BlogContent}");
-                Console.WriteLine("------------------------------------");
+                BlogId = Guid.NewGuid().ToString(),
+                BlogTitle = "Test Firebase Title edited",
+                BlogAuthor = "Test Firebase Author edited",
+                BlogContent = "Test Firebase Content edited"
+            };
+
+            //await blogService.AddBlogAsync(blog);
+            //await blogService.UpdateBlogAsync("-O78Tno7NMVTn0OPt0Zr", blog);
+            //await blogService.DeleteBlogAsync("-O78Tno7NMVTn0OPt0Zr");
+
+            var blogs = await blogService.GetBlogsAsync();
+            if (blogs is not null && blogs.Count > 0)
+            {
+                foreach (var item in blogs)
+                {
+                    Console.WriteLine($"Key: {item.Key}");
+                    Console.WriteLine($"Blog Id: {item.Value.BlogId}");
+                    Console.WriteLine($"Blog Title: {item.Value.BlogTitle}");
+                    Console.WriteLine($"Blog Author: {item.Value.BlogAuthor}");
+                    Console.WriteLine($"Blog Content: {item.Value.BlogContent}");
+                    Console.WriteLine("------------------------------------");
+                }
             }
         }
     }
